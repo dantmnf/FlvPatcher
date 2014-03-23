2c8b7ee84def0b488b8b628030515efe094b107f
dantmnf <dantmnf2@gmail.com>
2014-03-23 13:45:29 +0800

    initial commit

---
+++ blacker.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+# Blacker v0.01 by dant(dantmnf2 at gmail.com)
+# Licensed under WTFPL(http://www.wtfpl.net/txt/copying)
+# How to recover: delete the last 3 frames
+
+WORKDIR="/tmp/Blacker-$(uuidgen)"
+
+usage() {
+	echo "usage: $(basename $0) [-b target_bitrate] inputfile [outputfile]"
+}
+
+set_bitrate() { # $1: bitrate
+	target_bitrate=${1/[kK]/000:=990000}
+	target_bitrate=${target_bitrate:-990000}
+	echo "INFO:    target bitrate is ${target_bitrate} bps"
+}
+
+parse_commandline() {
+	while getopts 'b:' arg
+	do
+		echo $arg
+		case $arg in
+			'b')
+				br=$OPTARG
+				shift 2
+				;;
+			'?')
+				echo "WARNING: unknown argument: $arg" > 1
+				shift
+				;;
+		esac
+	done
+
+	inputfile=$1
+	if [ ! -e "$inputfile" ];then
+		usage
+		exit 1
+	fi
+	set_bitrate $br
+	outputfile=${2:-${inputfile}.upload.flv}
+}
+
+check() {
+	if [ ! -e $inputfile ];then
+		echo "ERROR:   input file '${inputfile}' does not exist!!"
+		exit 1
+	fi
+	if [ -e $outputfile ];then
+		echo -n "WARNING: output file '${outputfile}' exists, override? (y/N):"
+		read answer
+		if (( $answer != y && $answer != Y ));then
+			echo "INFO:    exiting..."
+			exit 1
+		fi
+		if rm -d -- $outputfile;then
+			echo "INFO:    overriding ${outputfile}"
+		else
+			echo "ERROR:   failed to override ${outputfile}"
+			exit 1
+		fi
+	fi
+
+
+}
+
+execute() {
+	mkdir -p "$WORKDIR"
+	# remux the input file to flv and measure the size
+	flvsize=$(ffmpeg -v 0 -i "$inputfile" -c copy -f flv - | wc -c)
+	if (( $flvsize == 0 ));then
+		echo "ERROR:   cannot analyze the input file."
+		echo "INFO:    please check whether ffmpeg is a recent version which can also remux"
+		echo "         media files, and make sure your input file can be remuxed to FLV."
+		exit 1
+	fi
+
+	# index the input file
+	ffmsindex -c "$inputfile" "$WORKDIR/tc" >/dev/null
+	tcfile="$WORKDIR/tc_track00.tc.txt"
+	if [ ! -e $tcfile ];then
+		echo "ERROR:   cannot analyze the input file."
+		echo "INFO:    please check whether a working ffmsindex is installed on your system"
+		exit 1
+	fi
+
+	# create a black patch
+	ffmpeg -v 0 -i "$inputfile" -c copy -frames:v 3 -- "$WORKDIR/patch.mkv"
+	patchsize=$(ffmpeg -v 0 -i "$WORKDIR/patch.mkv" -c copy -f flv - | wc -c)
+
+	# modify the timecode
+	blacktime1=$(echo "scale=8;((${flvsize}+${patchsize})/${target_bitrate})*8000" | bc)
+	blacktime2=$(echo "$blacktime1 + 60" | bc)
+	blacktime3=$(echo "$blacktime2 + 60" | bc)
+
+	echo $blacktime1 >> $tcfile
+	echo $blacktime2 >> $tcfile
+	echo $blacktime3 >> $tcfile
+
+	mkvmerge -o "$WORKDIR/upload.mkv" --timecodes "0:$tcfile" \
+	'(' "$inputfile" ')' '+' '(' "$WORKDIR/patch.mkv" ')'        \
+	--track-order "0:0,0:1" >/dev/null #--append-to "1:0:0:0" & >/dev/null
+
+	ffmpeg -v 0 -f matroska -i "$WORKDIR/upload.mkv" -c copy -f flv -y -- "$outputfile" 
+
+	# clean up
+	rm -rf -- $WORKDIR
+}
+
+parse_commandline $@
+check
+execute
